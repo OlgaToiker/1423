ed2a26f88d21c796df446def80e11185f4553bde
0lga <otoiker@yandex.ru>
2023-01-29 23:30:11 +0300

    Задали первоначальный массив; создали пустой массив, равный по длине первоначальному

--- Program.cs
+++ Program.cs
@@ -1,26 +1,3 @@
 ﻿string[] array1 = new string[16] { "У Лукоморья ", "дуб", " зелёный,", "Златая ", "цепь ", "на", " дубе ", "том", ", И днём,", "и ночью", "кот ", "учёный", "Всё", " ходит", "по цепи ", "кругом." };
 string[] array2 = new string[array1.Length];
 int n = 3;
-
-void SecondArray(string[] array1, string[] array2)
-{
-    int count = 0;
-    for (int i = 0; i < array1.Length; i++)
-    {
-        if (array1[i].Length <= n)
-        {
-            array2[count] = array1[i];
-            count++;
-        }
-    }
-}
-void PrintArray(string[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
-}
-SecondArray(array1, array2);
-PrintArray(array2);
